a37e563e56aa97ecaab7f5928fd85e44e652baab
Matthew.Dordal <Matthew.Dordal@target.com>
2016-05-10 08:56:41 -0500

    updated Hello.jsx

--- app/components/Hello.jsx
+++ app/components/Hello.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Hello(props) {
   return (
     <div className="title-shadow">
-      What's my mofo name {props.greet} {props.message}
+      {props.greet} {props.message}
     </div>
   );
 }
